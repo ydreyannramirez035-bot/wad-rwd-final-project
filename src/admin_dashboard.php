8d4d87a6e67433761158a50624013ff568bfa7d9
Ydrey Ann Ramirez <ydreyannramirez035@gmail.com>
2025-12-09 00:02:42 +0800

    update notif responsiveness

--- src/admin_dashboard.php
+++ src/admin_dashboard.php
@@ -234,7 +234,7 @@ $students_result = $stmt_students->execute();
                 </div>
                 <div class="dropdown">
                     <button class="btn btn-admin dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                        Admin • <?php echo htmlspecialchars(substr($user["username"], 0, 2)); ?>
+                        <span class="admin-text">Admin • </span><?php echo htmlspecialchars(substr($user["username"], 0, 2)); ?>
                     </button>
                     <ul class="dropdown-menu dropdown-menu-end">
                         <li class="px-3 py-1"><small>Signed in as<br><b><?php echo htmlspecialchars($user["username"]); ?></b></small></li>
